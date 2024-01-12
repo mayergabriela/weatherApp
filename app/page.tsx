28e975bab738a7771e3f7b0d626c104c6f5339da
Ga Mayer <mayergabriela@gmail.com>
2024-01-12 11:37:38 -0300

    Main page was created. Tremor library was added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+"use client";
+import { Card, Divider, Subtitle, Text } from "@tremor/react";
 
 export default function Home() {
   return (
-    <main className="text-6xl">
-      <h1>Let's build a GPT-4 Weather App</h1>
-    </main>
+    <div className="min-h-screen bg-gradient-to-br from-blue-950 to-blue-200">
+      <Card>
+        <Text className="text-6xl font-bold text-center mb-10">
+          Weather IA App
+        </Text>
+        <Subtitle className="text-xl text-center text-slate-500">
+          Powered by OpenAI, Next.Js, Tailwind CSS, Tremor 2.0, Stepten and
+          more!
+        </Subtitle>
+        <Divider className="my-10" />
+        <Card className="bg-gradient-to-br from-blue-950 to-blue-200">
+          {/* City Picker Component*/}
+        </Card>
+      </Card>
+    </div>
   );
 }
