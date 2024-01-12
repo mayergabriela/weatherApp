2580c183adfd825b43d996f58c342c45cf655b27
Ga Mayer <mayergabriela@gmail.com>
2024-01-12 17:58:02 -0300

    Apollo Client was installed

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,14 +1,13 @@
-import { ApolloServer } from "@apollo/server";
-import { InMemoryCache } from "@apollo/server";
-
-let client: ApolloServer<any> | null = null;
+//import { ApolloServer } from "@apollo/server";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const getClient = () => {
-  const client = new ApolloServer({
+  const client = new ApolloClient({
     uri: process.env.API_URL,
     cache: new InMemoryCache(),
     headers: {
       Authorization: `apikey ${process.env.NEXT_PUBLIC_STEPZEN_API_KEY}`,
     },
   });
+  return client;
 };
