e683c9460d337ac593c0088a936913a0605f7159
Ga Mayer <mayergabriela@gmail.com>
2024-01-14 09:05:12 -0300

    Query was added

--- graphql/queries/fetchWeatherQueries.tsx
+++ graphql/queries/fetchWeatherQueries.tsx
@@ -3,8 +3,8 @@ import { gql } from "@apollo/client";
 const fetchWeatherQuery = gql`
   query MyQuery(
     $current_weather: String
-    $daily: String = "weathercode, temperature_2m_max, temperature_2m_min, apparent_temperature_max, apparent_temperature_min,sunrise, sunset, uv_index_max, uv_index_clear_sky_max"
-    $hourly: String = "temperature_2m,relativehumidity_2m,apparent_temperature,precipitation_probability, precipitation,rain,showers,snowfall,snow_depth,windgusts_10m,uv_index_clear_sky"
+    $daily: String = "weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise sunset,uv_index_max,uv_index_clear_sky_max"
+    $hourly: String = "temperature_2m,relativehumidity_2m,apparent_temperature,precipitation_probability,precipitation,rain,showers,snowfall,snow_depth,windgusts_10m,uv_index_clear_sky"
     $latitude: String!
     $longitude: String!
     $timezone: String!
@@ -17,7 +17,15 @@ const fetchWeatherQuery = gql`
       longitude: $longitude
       timezone: $timezone
     ) {
+      latitude
+      longitude
+      timezone
+      timezone_abbreviation
+      utc_offset_seconds
+      elevation
+      generationtime_ms
       current_weather {
+        temperature
         interval
         is_day
         temperature
@@ -26,15 +34,37 @@ const fetchWeatherQuery = gql`
         winddirection
         windspeed
       }
-      current_weather_units {
-        interval
-        is_day
-        temperature
+      hourly {
+        apparent_temperature
+        precipitation_probability
+        precipitation
+        rain
+        relativehumidity_2m
+        showers
+        snow_depth
+        snowfall
+        temperature_2m
         time
-        weathercode
-        winddirection
-        windspeed
+        windgusts_10m
+        uv_index
+        uv_index_clear_sky
       }
+      hourly_units {
+        apparent_temperature
+        precipitation_probability
+        precipitation
+        rain
+        showers
+        relativehumidity_2m
+        snow_depth
+        snowfall
+        temperature_2m
+        time
+        uv_index
+        uv_index_clear_sky
+        windgusts_10m
+      }
+
       daily {
         apparent_temperature_max
         apparent_temperature_min
@@ -59,28 +89,7 @@ const fetchWeatherQuery = gql`
         uv_index_max
         weathercode
       }
-      elevation
-      generationtime_ms
-      hourly {
-        apparent_temperature
-        precipitation
-        precipitation_probability
-        rain
-        relativehumidity_2m
-        showers
-        snow_depth
-        snowfall
-        temperature_2m
-        time
-        uv_index
-        uv_index_clear_sky
-        windgusts_10m
-      }
-      latitude
-      longitude
-      timezone
-      timezone_abbreviation
-      utc_offset_seconds
     }
   }
 `;
+export default fetchWeatherQuery;
