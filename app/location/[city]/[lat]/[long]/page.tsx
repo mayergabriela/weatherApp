0118716ca1e314bd6cd5052e8477efa5026514a2
Ga Mayer <mayergabriela@gmail.com>
2024-01-16 12:36:07 -0300

    Status Charts were created . Styles were added.

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,6 +1,7 @@
 import { getClient } from "@/apollo-client";
 import fetchWeatherQuery from "@/graphql/queries/fetchWeatherQueries";
 import CalloutCard from "@/components/CalloutCard";
+import StatCard from "@/components/StatCard";
 
 type Props = {
   params: {
@@ -38,14 +39,61 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
               {results.timezone})
             </p>
           </div>
-          {/*Callout Card - It-s the GPT Summary */}
-          <div>
+
+          {/*Callout Card - It's the GPT Summary */}
+          <div className="m-2 mb-10">
             <CalloutCard message="This is where GPT Summary will go" />
+          </div>
+
+          {/*Temperature Stats */}
+          <div className="grid grid-cols-1 xl:grid-cols-2 gap-5 m-2 ">
+            <StatCard
+              title="Maximum Temperature"
+              metric={`${results.daily.temperature_2m_max[0].toFixed(1)}°C`}
+              color="yellow"
+            />
+
+            <StatCard
+              title="Minimum Temperature"
+              metric={`${results.daily.temperature_2m_min[0].toFixed(1)}°C`}
+              color="green"
+            />
+          </div>
+
+          {/*UV Stats */}
+          <div>
+            <StatCard
+              title="UV Index"
+              metric={`${results.daily.uv_index_max[0].toFixed(1)}`}
+              color="rose"
+            />
+            {Number(results.daily.uv_index_max[0].toFixed(1)) > 5 && (
+              <CalloutCard
+                message={"The UV is high today, so be sure to wear SPF!"}
+                warning
+              />
+            )}
+          </div>
+
+          {/*Wind Stats */}
+          <div className="flex space-x-3">
+            <StatCard
+              title="Wind Speed"
+              metric={`${results.current_weather.windspeed.toFixed(1)} m/s`}
+              color="cyan"
+            />
+            <StatCard
+              title="Wind Direction"
+              metric={`${results.current_weather.winddirection.toFixed(1)}°`}
+              color="violet"
+            />
           </div>
         </div>
       </div>
-      Welcome to the weather page: {city} {lat} {long}
-      {/*Information Pannel */}
+      <hr className='mb-5' />
+      <div>
+        
+      </div>
     </div>
   );
 }
